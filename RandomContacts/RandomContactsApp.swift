357c28b2780bdfe444e0b0fafea14d49b88d983a
bowski43 <43mngt@gmail.com>
2024-03-14 13:44:15 +0300

    Initial Commit

---
+++ RandomContacts/RandomContactsApp.swift
@@ -0,0 +1,17 @@
+//
+//  RandomContactsApp.swift
+//  RandomContacts
+//
+//  Created by Serge Broski on 3/14/24.
+//
+
+import SwiftUI
+
+@main
+struct RandomContactsApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
